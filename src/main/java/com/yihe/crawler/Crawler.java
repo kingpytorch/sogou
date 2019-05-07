d26b3e077401a9b5639ee9ebc5c6a55b5895e060
lexloo <lexloo@126.com>
2019-05-07 23:01:09 +0800

    支持简介搜索

--- src/main/java/com/yihe/crawler/Crawler.java
+++ src/main/java/com/yihe/crawler/Crawler.java
@@ -86,7 +86,7 @@ public class Crawler {
 
                 TimeUnit.SECONDS.sleep(this.getSpan());
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                // e.printStackTrace();
             }
         }
     }
@@ -152,22 +152,29 @@ public class Crawler {
                 }
             }
         } catch (Exception ex) {
-            // ex.printStackTrace();
+            ex.printStackTrace();
         }
     }
 
     private boolean crawlerPage(String keywords) {
         this.sleep(3);
 
-        List<WebElement> elems = this.driver.findElements(By.xpath("//*[@class=\"txt-box\"]/h3/a"));
-        for (WebElement elem : elems) {
+        List<WebElement> elems = this.driver.findElements(By.className("txt-box"));
+        for (WebElement txtBox : elems) {
+            WebElement elem = txtBox.findElement(By.xpath("./h3/a"));
             String caption = elem.getText();
-            String md5Caption = this.md5(caption);
 
             if (!caption.contains(keywords)) {
-                return false;
+                WebElement elemt = txtBox.findElement(By.className("txt-info"));
+                String captiont = elemt.getText();
+                if (!captiont.contains(keywords)) {
+                    return false;
+                } else {
+                    System.out.println(captiont);
+                }
             }
 
+            String md5Caption = this.md5(caption);
             boolean isExist = false;
             for (int i = 0; i < this.defaultModel.getRowCount(); i++) {
                 if (this.defaultModel.getValueAt(i, 3).equals(md5Caption)) {
@@ -191,23 +198,16 @@ public class Crawler {
         }
     }
 
-    // 写一个md5加密的方法
     private String md5(String plainText) {
-        // 定义一个字节数组
         byte[] secretBytes = null;
         try {
-            // 生成一个MD5加密计算摘要
             MessageDigest md = MessageDigest.getInstance("MD5");
-            // 对字符串进行加密
             md.update(plainText.getBytes());
-            // 获得加密后的数据
             secretBytes = md.digest();
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("没有md5这个算法！");
         }
-        // 将加密后的数据转换为16进制数字
-        String md5code = new BigInteger(1, secretBytes).toString(16);// 16进制数字
-        // 如果生成数字未满32位，需要前面补0
+        String md5code = new BigInteger(1, secretBytes).toString(16);
         for (int i = 0; i < 32 - md5code.length(); i++) {
             md5code = "0" + md5code;
         }
